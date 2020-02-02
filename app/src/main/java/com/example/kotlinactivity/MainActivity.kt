e99df296d1efc78b985f1f154bf7d6a7ef428710
myungeunpark <53125879+myungeunpark@users.noreply.github.com>
2020-02-02 00:21:31 -0800

    profile activity update

--- app/src/main/java/com/example/kotlinactivity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinactivity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
             val intent = Intent(this, SubActivity::class.java)
             intent.putExtra("name", "James Moon")
-            intent.putExtra("age", 20)
+            intent.putExtra("age", 12)
             intent.putExtra("school", "Fallon Middle School")
             intent.putExtra("score", 100)
             intent.putExtra("source", R.drawable.face1)
@@ -32,10 +33,11 @@ class MainActivity : AppCompatActivity() {
 
         if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
 
-            textView.text = data?.getStringExtra("name")
-            textView.append("\n + ${data?.getIntExtra("age", 0)}")
-            textView.append("\n + ${data?.getStringExtra("school")}")
-            textView.append("\n + ${data?.getIntExtra("score", 0)}")
+            textView1.text = "Profile Summary"
+            textView2.text = " + name - " + data?.getStringExtra("name")
+            textView2.append("\n + age - ${data?.getIntExtra("age", 0)}")
+            textView2.append("\n + school - ${data?.getStringExtra("school")}")
+            textView2.append("\n + score - ${data?.getIntExtra("score", 0)}")
         }
     }
 }
